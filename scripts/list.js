3024b3ba7e33173ac0a1bc900ea58a556d6ec212
Parker <adamssfd@gmail.com>
2023-03-12 20:19:07 +0300

    score system changed, also desgin tweaks made

---
+++ scripts/list.js
@@ -0,0 +1,78 @@
+var list = [
+    {
+        "name": "0",
+        "score": "0",
+    },
+    {
+        "name": "Khasuev Aslam",
+        "score": "119",
+    },
+    {
+        "name": "Dadaev Deni",
+        "score": "99",
+    },
+    {
+        "name": "Ulubaev Aslan",
+        "score": "64",
+    },
+    {
+        "name": "Dagaev Khamzat",
+        "score": "65",
+    },
+    {
+        "name": "Tagaev Muslim",
+        "score": "83",
+    },
+    {
+        "name": "Vitagrov Akhmed",
+        "score": "83",
+    },
+    {
+        "name": "Istamulova Eset",
+        "score": "37",
+    },
+    {
+        "name": "Hizriev Umar",
+        "score": "49",
+    },
+    {
+        "name": "Umarov Zaur",
+        "score": "25",
+    },
+    {
+        "name": "Baisultanov Muslim",
+        "score": "25",
+    },
+    {
+        "name": "Usuphadjiev Bai-Eli",
+        "score": "28",
+    },
+    {
+        "name": "Takaev Damir",
+        "score": "00",
+    },
+    {
+        "name": "Bikaev Magomed",
+        "score": "NA",
+    },
+    {
+        "name": "Aliev Askhab",
+        "score": "NA",
+    },
+]
+
+document.getElementById('G1').innerHTML = list[1].score;
+document.getElementById('G2').innerHTML = list[2].score;
+document.getElementById('G3').innerHTML = list[3].score;
+document.getElementById('G4').innerHTML = list[4].score;
+document.getElementById('G5').innerHTML = list[5].score;
+document.getElementById('G6').innerHTML = list[6].score;
+document.getElementById('G7').innerHTML = list[7].score;
+document.getElementById('G8').innerHTML = list[8].score;
+document.getElementById('G9').innerHTML = list[9].score;
+document.getElementById('G10').innerHTML = list[10].score;
+document.getElementById('G11').innerHTML = list[11].score;
+document.getElementById('G12').innerHTML = list[12].score;
+document.getElementById('G13').innerHTML = list[13].score;
+document.getElementById('G14').innerHTML = list[14].score;
+
